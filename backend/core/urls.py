d498238294aabec982240a6eb845876ec5c132dc
sjpbd <bro.racy19@gmail.com>
2026-05-12 12:57:20 +0600

    chore: move admin branding to api.apps.ready() for maximum reliability

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,11 +3,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# Customize Django Admin Header
-admin.site.site_header = "SJP Chapter 2027"
-admin.site.site_title = "SJP Chapter 2027 Admin Portal"
-admin.site.index_title = "Welcome to SJP Chapter 2027 Portal"
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
